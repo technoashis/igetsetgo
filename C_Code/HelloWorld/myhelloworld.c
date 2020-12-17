363bc8bd3a174e8837fdc7c2fb88724a5e8336e0
Ashis Dash <Ashis.Dash>
2020-12-17 17:05:42 +0530

    typo

--- C_Code/HelloWorld/myhelloworld.c
+++ C_Code/HelloWorld/myhelloworld.c
@@ -5,7 +5,7 @@ A simple program to print hello world in teh console.
 gcc myhelloWorld.c #to generate a.out !! Will Not Work !!
 gcc myhelloWorld.c -o out/myhelloworld.exe #to generate helloworld executable. !! Will Not Work !!
 gcc -c myhelloWorld.c -o out/myhelloworld.o #to generate helloworld object file.
-gcc helloWorld.c -L mylib/out/ -lmylib -o out/myhelloworld.exe
+gcc myhelloWorld.c -L mylib/out/ -lmylib -o out/myhelloworld.exe
 
 */
 #include <stdio.h>
